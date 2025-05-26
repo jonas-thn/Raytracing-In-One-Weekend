6147869c3ce1f501a74b50f2637c879053c31b5d
Jonas Thaun <thaunjonas@gmail.com>
2025-05-26 22:29:37 +0200

    color refactor and ray

--- Raytracing In One Weekend/main.cpp
+++ Raytracing In One Weekend/main.cpp
@@ -6,29 +6,26 @@
 #include <iostream>
 #include <vector>
 #include <cstdint>
+#include "Vec3.h"
 
 const int width = 256;
 const int height = 256;
 std::vector<uint8_t> framebuffer(width* height * 4);
 
-void Render()
+using Color = vec3;
+
+void WriteColor(const int x, const int y, const Color& pixel_color)
 {
-    for (int y = 0; y < height; ++y)
-    {
-        for (int x = 0; x < width; ++x)
-        {
-            int idx = (y * width + x) * 4;
-            framebuffer[idx + 0] = 255;
-            framebuffer[idx + 1] = 0;
-            framebuffer[idx + 2] = 0;
-            framebuffer[idx + 3] = (uint8_t)(255.0f * (float)x / width);
-        }
-    }
+    int idx = (y * width + x) * 3;
+     framebuffer[idx + 0] = uint8_t(pixel_color.x() * 255.99);
+     framebuffer[idx + 1] = uint8_t(pixel_color.y() * 255.99);
+     framebuffer[idx + 2] = uint8_t(pixel_color.z() * 255.99);
+
 }
 
-void ProduceImage()
+void Render()
 {
-    if (stbi_write_png("output.png", width, height, 4, framebuffer.data(), width * 4))
+    if (stbi_write_png("output.png", width, height, 3, framebuffer.data(), width * 3))
     {
         std::cout << "PNG written successfully.\n";
     }
@@ -40,8 +37,17 @@ void ProduceImage()
 
 int main() 
 {
+    for (int y = 0; y < height; ++y)
+    {
+        std::cout << "Scanline: " << y << " / " << height << std::endl;
+
+        for (int x = 0; x < width; ++x)
+        {
+			WriteColor(x, y, Color(x / (double)width, y / (double)height, 0.0));
+        }
+    }
+
     Render();
-    ProduceImage();
 
     return 0;
 }
